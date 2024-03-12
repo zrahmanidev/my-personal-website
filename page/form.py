ab5472b92780645175678854a2d5dee8dc28e569
Zahra Rahmani <z.rahmani2008@gmail.com>
2024-03-12 08:55:19 +0330

    [contact form] updated

---
+++ page/form.py
@@ -0,0 +1,51 @@
+from django import forms
+from captcha.fields import CaptchaField
+
+from core.components.CaptchaComponent import CustomCaptchaTextInput
+from page.models import ContactUs
+
+
+class ContactUsForm(forms.ModelForm):
+    name = forms.CharField(
+        label='نام و نام خانوادگی',
+        max_length=50,
+        error_messages={
+            'required': 'نام  و نام خانوادگی را وارد کنید',
+            'max_lenght': 'نام و نام خانوادگی نمیتواند بیشتر از 50 کاراکتر باشد'
+
+        },
+        widget=forms.TextInput(attrs={
+            'class': 'clear full',
+            'placeholder': 'نام و نام خانوادگی',
+            'required': 'required'
+        }),
+        required=True
+    )
+    subject = forms.CharField(label='عنوان', widget=forms.TextInput(attrs={
+        'class': 'clear full',
+        'placeholder': 'موضوع'
+    })
+                              )
+    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput(
+        attrs={
+            'class': 'clear full',
+            'placeholder': 'ایمیل'
+        }))
+    message = forms.CharField(label='متن پیام', widget=forms.Textarea(
+        attrs={
+            'class': 'clear full',
+            'placeholder': 'متن پیام',
+            # 'rows': 5
+            # 'id': 'message'
+        }
+    ))
+    captcha = CaptchaField(
+        widget=CustomCaptchaTextInput,
+        error_messages={'required': 'کد امنیتی را وارد کنید'},  # تغییر در این پیام خطا
+        label='کد امنیتی',  # اضافه کردن برچسب برای فیلد Captcha
+        required=True  # تنظیم فیلد Captcha به عنوان ضروری
+    )
+
+    class Meta:
+        model = ContactUs
+        fields = '__all__'
--- page/views.py
+++ page/views.py
@@ -1,19 +1,14 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from resume.models import About
 from django.views.generic.list import View
+from .form import ContactUsForm
+# from django_recaptcha.fields import ReCaptchaField
 from django.views.generic import CreateView
 
 from .models import ContactUs
 
 
-# from page import
-
-# Create your views ()
-# class home(CreateView):
-#     template_name = 'page/home.html'
-#     def get_context_data(self, **kwargs):
-#         context=super().get_context_data(*a)
 def home(request):
     about = About.objects.first()
     context = {'about': about,
@@ -25,26 +20,45 @@ def about(request):
     return render(request, 'about.html')
 
 
-class Contact(View):
-    model = ContactUs
-    template_name = 'contact.html'
-    context = {
-        'SEO_TITLE': 'contact'
-    }
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_page')
+    else:
+        form = ContactUsForm()
+    return render(request, 'contact.html', {'form': form})
 
-    def get(self, request):
-        return render(request, 'contact.html', )
+def contact_success_view(request):
+    return render(request, 'contact.html')
+# class Contact(View):
+#     model = ContactUs
+#     template_name = 'contact.html'
+#     form = ContactUsForm()
+#     context = {
+#         'SEO_TITLE': 'contact',
+#         'form': form
+#     }
 
-    def post(self, request):
-        request: HttpRequest = self.request
-        name = request.POST.get('name')
-        print(name)
-        # return render(request, 'contact.html', context)
-        contact = ContactUs.objects.create()
-        contact.name = request.POST.get('name')
-        contact.email = request.POST.get('email')
-        contact.subject = request.POST.get('subject')
-        contact.message = request.POST.get('message')
-        contact.save()
-
-        return render(request, 'contact.html', self.context)
+    # def get(self, request):
+    #     return render(request, 'contact.html', {'form': form})
+    #
+    # def post(self, request):
+    #     request: HttpRequest = self.request
+    #     name = request.POST.get('name')
+    #     print(name)
+    #     # return render(request, 'contact.html', context)
+    #
+    #     form = ContactUsForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('contact')
+        # contact = ContactUs.objects.create()
+        # contact.name = request.POST.get('name')
+        # contact.email = request.POST.get('email')
+        # contact.subject = request.POST.get('subject')
+        # contact.message = request.POST.get('message')
+        # contact.save()
+        # else:
+        #     return render(request, 'contact.html', self.context)
